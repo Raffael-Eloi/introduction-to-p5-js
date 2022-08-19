8435b1d5c1be78e97b34898f5e9f03e0f4ac0b42
Raffael Silva <raffaeleloi121@gmail.com>
2022-08-18 22:12:10 -0300

    initial structure

---
+++ sketch.js
@@ -0,0 +1,16 @@
+// Execute only one time
+function setup() {
+}
+
+// Execute a lot of times
+function draw() {
+    ellipse(50, 50, 80, 80);
+    
+    
+    // ellipse(x, y, w, h, detail);
+    // - x (Number): coordenate x of ellipse
+    // - y (Number): coordenate y of ellipse
+    // - w (Number): width of ellipse
+    // - h (Number): (optional) height of ellipse
+    // - detail (Integer): (optional) number of radial sectors to draw (modo WebGL)
+}
