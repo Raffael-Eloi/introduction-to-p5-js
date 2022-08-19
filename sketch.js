c67093c02339f70a892bee69f031cbf27bdbabd6
Raffael Silva <raffaeleloi121@gmail.com>
2022-08-18 22:36:03 -0300

    implement first draw

--- sketch.js
+++ sketch.js
@@ -1,11 +1,18 @@
+var x = 0;
+
 // Execute only one time
 function setup() {
+    createCanvas(400, 400);
 }
 
 // Execute a lot of times
 function draw() {
+    background('blue');
+    fill('green');
+
     ellipse(50, 50, 80, 80);
-    
+    ellipse(x, height/2, 80, 80);
+    x = x +1;
     
     // ellipse(x, y, w, h, detail);
     // - x (Number): coordenate x of ellipse
