ff6cb233c52811fcfb1d73cbacea26634b98946c
Raffael Silva <raffaeleloi121@gmail.com>
2022-08-18 22:57:09 -0300

    add first mouse manipulation draw

--- sketch.js
+++ sketch.js
@@ -1,23 +1,10 @@
-var x = 0;
-
-// Execute only one time
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(800, 800);
 }
 
-// Execute a lot of times
 function draw() {
-    background('blue');
-    fill('green');
+    if (mouseIsPressed) fill('yellow');
+    else fill('blue');
 
-    ellipse(50, 50, 80, 80);
-    ellipse(x, height/2, 80, 80);
-    x = x +1;
-    
-    // ellipse(x, y, w, h, detail);
-    // - x (Number): coordenate x of ellipse
-    // - y (Number): coordenate y of ellipse
-    // - w (Number): width of ellipse
-    // - h (Number): (optional) height of ellipse
-    // - detail (Integer): (optional) number of radial sectors to draw (modo WebGL)
+    ellipse(mouseX, mouseY, 80, 80);
 }
