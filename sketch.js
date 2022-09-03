ff55c1a0dfea75832b97fcd5c9a4392734e47f27
Raffael Silva <raffaeleloi121@gmail.com>
2022-09-02 21:13:22 -0300

    add base model

--- sketch.js
+++ sketch.js
@@ -1,57 +1,80 @@
-var x;
-var y;
-var w;
-var h;
-var pi = 3.14;
-var initialPosition = 3 * pi / 2;
-var start = initialPosition + 0.1;
-var stopSecond = initialPosition + 0.1;
-var stopMinute = initialPosition + 0.1;
-var stopHours = initialPosition + 0.1;
-var stopCondition = pi*4 - 1.5
+var raio = 200;
+var cx, cy;
+var px, py;
+var destaque_theta;
 
 function setup() {
   createCanvas(600, 600);
   textAlign(CENTER, CENTER);
-  x = width / 2;
-  y = height / 2;
-  w = 400;
-  h = 400;
-  secondIncrement = 0.1/60;
-  minuteIncrement = secondIncrement / 60;
-  hourIncrement = minuteIncrement / 60;
-}
+  // centro do desenho e do circulo
+  cx = width / 2;
+  cy = height / 2;
 
-function arcOfSeconds () {
-    arc(x, y, w-20, h-20, initialPosition, stopSecond);
-    if (stopSecond < stopCondition) {
-        stopSecond += secondIncrement;
-    } else stopSecond = start;
-}
-
-function arcOfMinutes () {
-    arc(x, y, w-70, h-70, initialPosition, stopMinute);
-    if (stopMinute < stopCondition) {
-        stopMinute += minuteIncrement;
-    } else stopMinute = start;
-}
-
-function arcOfHours () {
-    arc(x, y, w-120, h-120, initialPosition, stopHours);
-    if (stopHours < stopCondition) {
-        stopHours += hourIncrement;
-    } else stopHours = start;
+  // ângulo theta para arco de destaque
+  // que é o mesmo theta para o ponto p
+  destaque_theta = PI * 1.75;
+  // coordenada do ponto p
+  // transformado de coord polar para coord cartesiana
+  // theta = PI * 1.75
+  px = cos(destaque_theta) * raio;
+  py = sin(destaque_theta) * raio;
+  // corrige p(px,py) para p(px+cx,py+cy);
+  px += cx;
+  py += cy;
 }
 
 function draw() {
-  background("pink");
-  ellipse(x, y, w+10);
-  arcOfSeconds();
-  arcOfMinutes();
-  arcOfHours();
-  text('Relógio com arcos', x, y/6);
-  text('12', x, y/4);
-  text('9', x/4, y);
-  text('6', x, y+250);
-  text('3', x+250, y);
+  background(220);
+
+  fill(255);
+  var inc = 360/60;
+  for (var angulo = 0; angulo <= 360; angulo += inc) {
+    // transforma o angulo de
+    // coord polar para coord cartesiana
+    var x = cos(radians(angulo)) * raio;
+    var y = sin(radians(angulo)) * raio;
+
+    // desenha uma elipse na coord (x,y)
+    // corrige (x,y) para (x+cx,y+cy)
+    ellipse(x+cx, y+cy, 5);
+  }
+
+  // desenha uma elipse na coord (cx,cy)
+  ellipse(cx, cy, 5);
+
+  // preenchimento preto para o texto
+  fill(0);
+  // texto da coord (cx,cy)
+  text('C\n(200,200)', cx, cy + 20);
+
+  // linha de (cx,cy) a (cx+raio, cy)
+  line(cx, cy, cx + raio, cy);
+
+  // preenchimento branco
+  fill(255);
+  // arco para a área de destaque no círculo
+  var wh = raio * 2;
+  arc(cx, cy, wh, wh, destaque_theta, 0);
+
+  // elipse para destacar o ponto p
+  ellipse(px, py, 10);
+
+  // texto do ponto p
+  fill(0);
+  text('p(r,theta)', px + 35, py - 5);
+
+  // linha de (cx,cy) até o ponto p
+  line(cx, cy, px, py);
+
+  // arco para destacar o ângulo formado
+  // pelo triângulo interno ao círculo
+  noFill();
+  arc(cx, cy, 50, 50, destaque_theta, 0);
+
+  // texto 'theta' (ângulo)
+  fill(0);
+  text('theta', cx + 45, cy - 15);
+
+  // texto 'r' (raio)
+  text('r = ' + raio, cx + raio / 2, cy + 15);
 }
